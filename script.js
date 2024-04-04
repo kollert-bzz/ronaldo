2e133c83b78594dc08d16c3e349c6e5ad764aa26
kollert-bzz <kollert@bzz.ch>
2024-04-04 19:46:14 +0200

    4.4 - 19:00

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ document.addEventListener("DOMContentLoaded", function() {
     if (instagramLink) {
         const instagramImg = instagramLink.querySelector('img');
 
-        instagramLink.addEventListener('mouseover', function() {
+        instagramLink.addEventListener('mouseover', function () {
             instagramImg.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/2048px-Instagram_logo_2016.svg.png';
         });
 
-        instagramLink.addEventListener('mouseout', function() {
+        instagramLink.addEventListener('mouseout', function () {
             instagramImg.src = 'https://www.edigitalagency.com.au/wp-content/uploads/new-Instagram-logo-black-glyph.png';
         });
     }
@@ -16,11 +16,11 @@ document.addEventListener("DOMContentLoaded", function() {
     if (twitterLink) {
         const twitterImg = twitterLink.querySelector('img');
 
-        twitterLink.addEventListener('mouseover', function() {
+        twitterLink.addEventListener('mouseover', function () {
             twitterImg.src = 'https://upload.wikimedia.org/wikipedia/commons/5/57/X_logo_2023_%28white%29.png';
         });
 
-        twitterLink.addEventListener('mouseout', function() {
+        twitterLink.addEventListener('mouseout', function () {
             twitterImg.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/X_logo_2023_original.svg/2048px-X_logo_2023_original.svg.png';
         });
     }
@@ -29,11 +29,11 @@ document.addEventListener("DOMContentLoaded", function() {
     if (facebookLink) {
         const facebookImg = facebookLink.querySelector('img');
 
-        facebookLink.addEventListener('mouseover', function() {
+        facebookLink.addEventListener('mouseover', function () {
             facebookImg.src = 'https://upload.wikimedia.org/wikipedia/commons/b/b9/2023_Facebook_icon.svg';
         });
 
-        facebookLink.addEventListener('mouseout', function() {
+        facebookLink.addEventListener('mouseout', function () {
             facebookImg.src = 'https://allfacebook.de/wp-content/uploads/2020/02/f_logo_RGB-Black_1024.png';
         });
     }
@@ -42,11 +42,11 @@ document.addEventListener("DOMContentLoaded", function() {
     if (cr7_websiteLink) {
         const cr7_websiteImg = cr7_websiteLink.querySelector('img');
 
-        cr7_websiteLink.addEventListener('mouseover', function() {
+        cr7_websiteLink.addEventListener('mouseover', function () {
             cr7_websiteImg.src = 'https://static.vecteezy.com/system/resources/previews/010/994/486/non_2x/cr7-symbol-logo-white-clothes-design-icon-abstract-football-illustration-with-black-background-free-vector.jpg';
         });
 
-        cr7_websiteLink.addEventListener('mouseout', function() {
+        cr7_websiteLink.addEventListener('mouseout', function () {
             cr7_websiteImg.src = 'https://static.vecteezy.com/system/resources/previews/010/994/282/non_2x/cr7-logo-symbol-black-clothes-design-icon-abstract-football-illustration-with-white-background-free-vector.jpg';
         });
     }
@@ -59,7 +59,7 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     if (logoLink) {
-        logoLink.addEventListener('mouseover', function() {
+        logoLink.addEventListener('mouseover', function () {
             if (window.location.pathname === '/ronaldo/index.html') {
                 logoImg.src = 'img/logo_navbar.jpg';
             } else {
@@ -67,7 +67,7 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         });
 
-        logoLink.addEventListener('mouseout', function() {
+        logoLink.addEventListener('mouseout', function () {
             if (window.location.pathname === '/ronaldo/index.html') {
                 logoImg.src = 'img/logo_navbar_orange.jpg';
             } else {
@@ -93,4 +93,7 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     setInterval(nextSlide, 5000);
+
+
+
 });
